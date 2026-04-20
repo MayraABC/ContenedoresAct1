18044e4e1c331eb0deb2de3a9ca969decec0a41c
MayraABC <62311267+MayraABC@users.noreply.github.com>
2026-04-20 10:03:02 -0500

    Se modifica ip pública

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://13.52.217.95:3001';
+const API_URL = process.env.REACT_APP_API_URL || 'http://52.53.211.160:3001';
 
 function App() {
   const [usuarios, setUsuarios] = useState([]);
