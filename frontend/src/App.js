aaed60aa7a10db89722637cae713492315db8638
MayraABC <62311267+MayraABC@users.noreply.github.com>
2026-05-07 18:29:47 -0500

    Ajuste de ip pública.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://3.101.152.81:3001';
+const API_URL = process.env.REACT_APP_API_URL || 'http://54.183.114.205:3001';
 
 function App() {
   const [usuarios, setUsuarios] = useState([]);
